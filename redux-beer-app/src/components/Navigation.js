41b9a52ebd2c93fbffcfdbf2ec331e8b5b66d5ad
amxra <amiradediran@gmail.com>
2019-10-10 23:49:26 +0100

    Create navigation component

---
+++ redux-beer-app/src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+ function Navigation(){
+     return(
+         <nav>
+             <h2>Brewdog Beer Recipes</h2>
+             <p>Beers</p>
+             <p>FindBeer</p>
+         </nav>
+     )
+ }
+
+ export default Navigation;
