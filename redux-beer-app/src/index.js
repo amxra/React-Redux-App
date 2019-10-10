0242d2549c23a6d01043c3fb55ff0b982182cc34
amxra <amiradediran@gmail.com>
2019-10-10 23:19:10 +0100

    Combine reducer, create stor and inject store into app

--- redux-beer-app/src/index.js
+++ redux-beer-app/src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+ import ReactDOM from 'react-dom';
+ import { combineReducers, createStore} from 'redux';
+ import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ import * as reducers from './state/reducers';
+ import './index.css';
+ import App from './App';
+ import * as serviceWorker from './serviceWorker';
+
+ ReactDOM.render(<App />, document.getElementById('root'));
+ const monsterReducer = combineReducers({
+     beers: reducers.beerReducer,
+ })
+
+ const store = createStore(
+     monsterReducer,
+     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+     )
+
+
+ ReactDOM.render(
+     <Provider store={store}>
+         <App />
+     </Provider>
+ , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
