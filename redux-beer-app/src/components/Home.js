409e1d77fafa8411dc929061c101080cf376ed45
amxra <amiradediran@gmail.com>
2019-10-10 23:47:17 +0100

    Create home component

---
+++ redux-beer-app/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+ function Home(){
+     return (
+         <div class='home'>
+         <h1>Welcome to Beer recipes page</h1>
+         <img src={'https://image.freepik.com/free-photo/beer-bottles-frame-wheat-with-copy-space_23-2148306003.jpg'} alt={'beer homepage'}/>
+     </div>
+     )
+ }
+
+ export default Home;
