042b724a169af882a5b0d366a0395312045495fb
amxra <amiradediran@gmail.com>
2019-10-10 23:22:11 +0100

    render beers in app.js

--- redux-beer-app/src/App.js
+++ redux-beer-app/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Beers from './components/Beers';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Beers/>
     </div>
   );
 }
